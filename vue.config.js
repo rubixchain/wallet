5b92aa2edc9ffedb09a0f076aa214a62d1ea106f
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2022-10-25 12:22:33 +0530

    Update Rubix Jar - Eismeer 2.4.0

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-    devServer: {
-        port: 8081
-    },
-    publicPath: '/',
-    pluginOptions: {
-        electronBuilder: {
-            builderOptions: {
-                extraFiles: {
-                    from: 'src/extraResources/rubix.jar',
-                    to: './resources/app.asar.unpacked/rubix.jar',
-                    filter: ["**/*"]
-                },
-                productName: "Rubix Wallet",
-                appId: 'rubix.network',
-                win: {
-                    "target": [
-                        "nsis"
-                    ],
-                    icon: 'public/favicon.png',
-                    "requestedExecutionLevel": "requireAdministrator"
-                },
-                "nsis": {
-                    // "installerIcon": "favicon.ico",
-                    // "uninstallerIcon": "favicon.ico",
-                    "uninstallDisplayName": "Rubix Wallet",
-                    "license": "license.txt",
-                    "oneClick": true,
-                    "allowToChangeInstallationDirectory": true
-                }
-            },
-        },
-    },
+  devServer: {
+      port: 8081
+  },
+  publicPath: '/',
+  pluginOptions: {
+      electronBuilder: {
+          builderOptions: {
+              extraFiles: {
+                  from: 'src/extraResources/rubix.jar',
+                  to: './resources/app.asar.unpacked/rubix.jar',
+                  filter: ["**/*"]
+              },
+              productName: "Rubix Wallet - Eismeer",
+              appId: 'rubix.network',
+              win: {
+                  "target": [
+                      "nsis"
+                  ],
+                  icon: 'public/favicon.png',
+                  "requestedExecutionLevel": "requireAdministrator"
+              },
+              "nsis": {
+                  // "installerIcon": "favicon.ico",
+                  // "uninstallerIcon": "favicon.ico",
+                  "uninstallDisplayName": "Rubix Wallet",
+                  "license": "license.txt",
+                  "oneClick": true,
+                  "allowToChangeInstallationDirectory": true
+              }
+          },
+      },
+  },
 };
